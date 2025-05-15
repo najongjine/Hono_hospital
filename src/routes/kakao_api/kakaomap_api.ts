4591928571525aaf24cebab7eff656f6069d948b
najongjine <najongjin3@hotmail.com>
2025-05-15 09:56:23 +0900

    .

--- src/routes/kakao_api/kakaomap_api.ts
+++ src/routes/kakao_api/kakaomap_api.ts
@@ -25,16 +25,20 @@ router.get("/search_keyword", async (c) => {
 			throw new Error('!!! KAKAO_REST_API_KEY is not defined in environment variables.');
 		}
 		const url = 'https://dapi.kakao.com/v2/local/search/keyword.json';
-		let query = String(c ?. req ?. query('query') ?? "피부과 병원");
+		let query = String(c ?. req ?. query('query') ?? "피부과");
 		let long = String(c.req.query('long') ?? "126.80141560943788"); // 경도. x
 		let lang = String(c.req.query('lang') ?? "35.17832759507573"); // 위도 y
 		let radius = String(c.req.query('radius') ?? "2000");
+		let category_group_code = String(c.req.query('category_group_code') ?? "HP8");
+		let sort = String(c.req.query('sort') ?? "distance");
 
 		const params = {
 			query: query,
 			x: long,
 			y: lang,
-			radius: radius
+			radius: radius,
+			category_group_code: category_group_code, // 병원 코드
+			sort: sort
 		};
 
 		const headers = {
