ff4e34202d6fb2f28e6750de212f7a1251fc1097
najongjine <najongjin3@hotmail.com>
2025-05-19 07:08:20 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ dotenv.config({ path: envFile });
 /** import routes */
 import test1 from "./routes/test1/test1.js";
 import dataapi from "./routes/dataapi/dataapi.js";
+import kakaomap_api from "./routes/kakao_api/kakaomap_api.js";
 /** import routes END */
 
 const app = new Hono();
@@ -29,6 +30,7 @@ app.get("/", (c) => {
 
 app.route("/test1", test1);
 app.route("/dataapi", dataapi);
+app.route("/kakaomap_api", kakaomap_api);
 
 app.onError((err, c) => {
   return c.json({ success: false, data: null, code: "global_err", message: `${err?.message ?? "!!!_global_err"}` });
---
+++ src/routes/kakao_api/kakaomap_api.ts
@@ -0,0 +1,62 @@
+import { Hono } from "hono";
+import { AppDataSource } from "../../data-source1";
+import { TStores } from "../../entities/TStores";
+import axios from "axios";
+import { TMedicalSpecCode } from "../../entities/TMedicalSpecCode";
+import { THospital } from "../../entities/THospital";
+import { THospitalMedicalcodes } from "../../entities/THospitalMedicalcodes";
+import { Code } from "typeorm";
+import * as KakaoTypes from "./kakaomap_api_type";
+
+const router = new Hono();
+
+router.get("/search_keyword", async (c) => {
+  let result: { success: boolean; data: any; code: string; message: string } = {
+    success: true,
+    data: null,
+    code: "",
+    message: ``,
+  };
+  try {
+    const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
+    if (!KAKAO_REST_API_KEY) {
+      result.success = false;
+      throw new Error("!!! KAKAO_REST_API_KEY is not defined in environment variables.");
+    }
+    const url = "https://dapi.kakao.com/v2/local/search/keyword.json";
+    let query = String(c?.req?.query("query") ?? "피부과");
+    let long = String(c.req.query("long") ?? "126.80141560943788"); // 경도. x
+    let lang = String(c.req.query("lang") ?? "35.17832759507573"); // 위도 y
+    let radius = String(c.req.query("radius") ?? "2000");
+    let category_group_code = String(c.req.query("category_group_code") ?? "HP8");
+    let sort = String(c.req.query("sort") ?? "distance");
+
+    const params = {
+      query: query,
+      x: long,
+      y: lang,
+      radius: radius,
+      category_group_code: category_group_code, // 병원 코드
+      sort: sort,
+    };
+
+    const headers = {
+      Authorization: `KakaoAK ${KAKAO_REST_API_KEY}`,
+    };
+
+    const response = await axios.get(url, { params, headers });
+
+    const places: KakaoTypes.KakaoKeywordSearchResponse = response?.data;
+
+    console.log("검색 결과:", places);
+
+    result.data = places;
+
+    return c.json(result);
+  } catch (error: any) {
+    result.success = false;
+    result.message = error?.message;
+    return c.json(result);
+  }
+});
+export default router;
